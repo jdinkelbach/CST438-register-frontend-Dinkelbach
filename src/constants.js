ffdefe551dc536bffe6e7d93a698065c8386398b
Justin <jmdinkelbach@gmail.com>
2021-10-02 08:25:36 -0700

    Updated backend url

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-export const SERVER_URL = 'https://git.heroku.com/cst438register-be.git';
+export const SERVER_URL = 'https://cst438register-be.herokuapp.com/';
 
 export const SEMESTER_LIST = [
   {id:0, year:2020, name:'Fall'},
   {id:1, year:2021, name:'Spring'},
   {id:2, year:2021, name:'Fall'}
-] ;
+] ; 
